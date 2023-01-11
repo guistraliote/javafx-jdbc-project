e2184ac7b95c9d8b85c3f413a2d993db7403b652
Guilherme Straliote <gui.ssh@gmail.com>
2023-01-11 20:00:26 -0300

    DepartmentList view design

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -32,7 +32,7 @@ public class MainViewController implements Initializable {
 	
 	@FXML
 	public void onMenuItemDepartmentAction() {
-		System.out.println("onMenuDepartmentAction");
+		loadView("/gui/DepartmentList.fxml");;
 	}
 	
 	@FXML
@@ -41,7 +41,7 @@ public class MainViewController implements Initializable {
 	}
 	
 	@Override
-	public void initialize(URL uri, ResourceBundle rb) {	
+	public synchronized void initialize(URL uri, ResourceBundle rb) {	
 	}
 	
 	private void loadView(String absoluteName) {
