358fac5a9f8f418550b6265bb1cd8d75cbfb4403
Guilherme Straliote <gui.ssh@gmail.com>
2023-01-12 19:11:49 -0300

    Department service

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,35 +1,50 @@
 package gui;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
 import gui.util.Alerts;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import model.entities.Department;
+import model.services.DepartmentService;
 
-public class DepartmentListController implements Initializable{
+public class DepartmentListController implements Initializable {
+	
+	private DepartmentService service;
 
 	@FXML
 	private TableView<Department> tableViewDepartment;
+	
 	@FXML
 	private TableColumn<Department, Integer> tableColumnId;
+	
 	@FXML
 	private TableColumn<Department, String> tableColumnName;
+	
 	@FXML
 	private Button btNew;
+	
+	private ObservableList<Department> obsList;
 	
 	@FXML
 	public void onBtNewAction() {
 		// System.out.println("onBtNewAction");
 		Alerts.showAlert("Null", "Null", null, AlertType.ERROR);
+	}
+	
+	public void setDepartmentService(DepartmentService service) {
+		this.service = service;
 	}
 	
 	@Override
@@ -43,5 +58,14 @@ public class DepartmentListController implements Initializable{
 		
 		Stage stage = (Stage) Main.getMainScene().getWindow();
 		tableViewDepartment.prefHeightProperty().bind(stage.heightProperty());
+	}
+	
+	public void updateTableView() {
+		if (service == null) {
+			throw new IllegalStateException("Serivice was null");
+		}
+		List<Department> list = service.findAll();
+		obsList = FXCollections.observableArrayList(list);
+		tableViewDepartment.setItems(obsList);
 	}
 }
